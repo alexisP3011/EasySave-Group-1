887fddb9ae3e1eba5ca9a4515dba6a84dae36d47
alexisP3011 <alexis.peiffer@viacesi.fr>
2025-05-07 20:11:14 +0200

    add method in Add class

--- Version_1.0/View/Add.cs
+++ Version_1.0/View/Add.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Version_1._0.View
 {
-    internal class Add
+    class Add : View
     {
+        public Add() { }
+
+        public void AskSaveName()
+        {
+            Console.WriteLine("Please enter the name of the backup work you want to create: ");
+        }
+
+        public void AskSource()
+        {
+            Console.WriteLine("Please enter your source: ");
+        }
+        public void AskTarget()
+        {
+            Console.WriteLine("Please enter your target: ");
+        }
+
+        public void AskType()
+        {
+            Console.WriteLine("Please enter the type of backup work you want to create: ");
+        }
+
+
     }
 }
