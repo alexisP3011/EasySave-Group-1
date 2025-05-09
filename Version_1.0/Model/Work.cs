79e76a9d32c08a81f13f2c0a125dbf06bf497c7d
Clement <clement.evangelisti@viacesi.fr>
2025-05-09 11:08:55 +0200

    modify view-model and model to suit mvvm

---
+++ Version_1.0/Model/Work.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version_1._0.Model
+{
+    public class Work
+    {
+        private string name;
+        private string source;
+        private string target;
+        private string type;
+        private string state;
+        
+        public String GetName()
+        {
+            return this.name;
+        }
+        public void SetName(string input)
+        {
+            this.name = input;
+        }
+        public String GetSource()
+        {
+            return this.source;
+        }
+        public void SetSource(string input)
+        {
+            this.source = input;
+        }
+        public String GetTarget()
+        {
+            return this.target;
+        }
+        public void SetTarget(string input)
+        {
+            this.target = input;
+        }
+        public String GetType()
+        {
+            return this.type;
+        }
+        public void SetType(string input)
+        {
+            this.type = input;
+        }
+        public String GetState()
+        {
+            return this.state;
+        }
+        public void SetState(string input)
+        {
+            this.state = input;
+        }
+
+
+
+
+        public Work() {
+            name = "";
+            source = "";
+            target = "";
+            type = "";
+            state = "inactive";     
+
+        }
+
+        ~Work() // Destructor
+        {            
+        }
+        
+
+
+    }
+}
--- Version_1.0/ViewModel/ViewModel.cs
+++ Version_1.0/ViewModel/ViewModel.cs
@@ -10,6 +10,7 @@ namespace Version_1._0.ViewModel
     {
         // Collection de travaux
         private List<Model.Work> _works = new List<Model.Work>();
+
         // Travail actuel pour les opérations
         private Model.Work _currentWork = new Model.Work();
 
@@ -22,68 +23,69 @@ namespace Version_1._0.ViewModel
         public string output { get; set; }
 
         // Méthodes pour le travail actuel
-        public string GetName()
+        public void GetName()
         {
-            return output = _currentWork.name;
+            output = _currentWork.GetName();
         }
 
         public void SetName(string input)
         {
-            _currentWork.name = input;
+            _currentWork.SetName(input);
         }
 
-        public string GetSource()
+        public void GetSource()
         {
-            return output = _currentWork.source;
+            output = _currentWork.GetSource();
         }
         public void SetSource(string input)
         {
-            _currentWork.source = input;
+            _currentWork.SetSource(input);
         }
-        public string GetTarget()
+        public void GetTarget()
         {
-            return output = _currentWork.target;
+            output = _currentWork.GetTarget();
         }
         public void SetTarget(string input)
         {
-            _currentWork.target = input;
+            _currentWork.SetTarget(input);
         }
 
-        public string GetType()
+        public void GetType()
         {
-            return output = _currentWork.type;
+            output = _currentWork.GetType();
         }
         public void SetType(string input)
         {
-            _currentWork.type = input;
+            _currentWork.SetType(input);
         }
 
-        public string GetState()
+        public void GetState()
         {
-            return output = _currentWork.state;
+            output = _currentWork.GetState();
         }
 
-        public string SetState(string input)
+        public void SetState(string input)
         {
-            _currentWork.state = input;
-            return _currentWork.state;
+            _currentWork.SetState(input);
         }
 
        
         public void AddWork()
         {
+            // Create a new instance of Model.Work
+            Model.Work newWork = new Model.Work();
 
-            Model.Work newWork = new Model.Work
-            {
-                name = _currentWork.name,
-                source = _currentWork.source,
-                target = _currentWork.target,
-                type = _currentWork.type,
-                state = _currentWork.state
-            };
+            // Set properties of the new work using the current work
+            newWork.SetName(_currentWork.GetName());
+            newWork.SetSource(_currentWork.GetSource());
+            newWork.SetTarget(_currentWork.GetTarget());
+            newWork.SetType(_currentWork.GetType());
+            newWork.SetState(_currentWork.GetState());
 
+            // Add the new work to the collection
             _works.Add(newWork);
 
+            // Reset the current work
             _currentWork = new Model.Work();
         }
 
@@ -94,12 +96,12 @@ namespace Version_1._0.ViewModel
 
         public Model.Work GetWorkByName(string name)
         {
-            return _works.FirstOrDefault(w => w.name == name);
+            return _works.FirstOrDefault(w => w.GetName() == name);
         }
 
         public bool DeleteWork(string name)
         {
-            Model.Work workToDelete = _works.FirstOrDefault(w => w.name == name);
+            Model.Work workToDelete = _works.FirstOrDefault(w => w.GetName() == name);
             if (workToDelete != null)
             {
                 return _works.Remove(workToDelete);
@@ -112,18 +114,18 @@ namespace Version_1._0.ViewModel
             Model.Work work = GetWorkByName(name);
             if (work != null)
             {
-                _currentWork.name = work.name;
-                _currentWork.source = work.source;
-                _currentWork.target = work.target;
-                _currentWork.type = work.type;
-                _currentWork.state = work.state;
+                _currentWork.SetName(work.GetName());
+                _currentWork.SetSource(work.GetSource());
+                _currentWork.SetTarget(work.GetTarget());
+                _currentWork.SetType(work.GetType());
+                _currentWork.SetState(work.GetState());
             }
         }
 
         public void UpdateWork(string name)
         {
 
-            Model.Work workToUpdate = _works.FirstOrDefault(w => w.name == name);
+            Model.Work workToUpdate = _works.FirstOrDefault(w => w.GetName() == name);
             if (workToUpdate != null)
             {
 
@@ -139,28 +141,28 @@ namespace Version_1._0.ViewModel
 
         public void LaunchWork(Model.Work workToLaunch)
         {
-            if (workToLaunch.state == "inactive")
+            if (workToLaunch.GetState() == "inactive")
             {
-                workToLaunch.state = "active";
+                workToLaunch.SetState("active");
 
                 // Verify if the source directory exists
-                if (Directory.Exists(workToLaunch.source))
+                if (Directory.Exists(workToLaunch.GetSource()))
                 {
                     // Create the target directory if it doesn't exist
-                    if (!Directory.Exists(workToLaunch.target))
+                    if (!Directory.Exists(workToLaunch.GetTarget()))
                     {
-                        Directory.CreateDirectory(workToLaunch.target);
+                        Directory.CreateDirectory(workToLaunch.GetTarget());
                     }
 
                     // Copy all of the files to the new location
-                    foreach (var file in Directory.GetFiles(workToLaunch.source))
+                    foreach (var file in Directory.GetFiles(workToLaunch.GetSource()))
                     {
                         string fileName = Path.GetFileName(file);
-                        string destFile = Path.Combine(workToLaunch.target, fileName);
+                        string destFile = Path.Combine(workToLaunch.GetTarget(), fileName);
                         File.Copy(file, destFile, overwrite: true);
                     }
                 }
-                workToLaunch.state = "finished";
+                workToLaunch.SetState("finished");
 
                 //_works.Remove(workToLaunch);
             }
