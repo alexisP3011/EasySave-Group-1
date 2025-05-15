5bcee207c04a531f842e8c0eccbcaee8fb8b3fb1
alexisP3011 <alexis.peiffer@viacesi.fr>
2025-05-15 12:21:28 +0200

    xaml files for the view

---
+++ Version_2.0/View/PopUp/ConfirmationPopUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Version_2._0.View.PopUp
+{
+    /// <summary>
+    /// Logique d'interaction pour ConfirmationPopUp.xaml
+    /// </summary>
+    public partial class ConfirmationPopUp : Window
+    {
+        public ConfirmationPopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
