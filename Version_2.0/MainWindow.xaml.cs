a32e9a6dc47ef100fab81e40614bc24e0e2cf1fd
alexisP3011 <alexis.peiffer@viacesi.fr>
2025-05-16 12:27:06 +0200

    we can create an objet work but it's inside the code not with gui

--- Version_2.0/MainWindow.xaml.cs
+++ Version_2.0/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +12,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Version_2._0.View;
 using Version_2._0.View.PopUp;
+using System.ComponentModel;
 
 namespace Version_2._0
 {
@@ -21,8 +24,95 @@ namespace Version_2._0
         public MainWindow()
         {
             InitializeComponent();
+
+            Work work = new Work
+            {
+                Name = "Test",
+                Source = @"C:\Users\andre\Desktop\test",
+                Target = @"C:\Users\andre\Desktop\test2",
+                Type = "copy",
+                State = "inactive"
+            };
+
+            this.DataContext = work;
+        }
+    }
+
+
+
+
+    public class Work : INotifyPropertyChanged
+        {
+            private string name;
+            private string source;
+            private string target;
+            private string type;
+            private string state;
+
+            public string Name
+            {
+                get => name;
+                set
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+
+            public string Source
+            {
+                get => source;
+                set
+                {
+                    source = value;
+                    OnPropertyChanged(nameof(Source));
+                }
+            }
+
+            public string Target
+            {
+                get => target;
+                set
+                {
+                    target = value;
+                    OnPropertyChanged(nameof(Target));
+                }
+            }
+
+            public string Type
+            {
+                get => type;
+                set
+                {
+                    type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+
+            public string State
+            {
+                get => state;
+                set
+                {
+                    state = value;
+                    OnPropertyChanged(nameof(State));
+                }
+            }
+
+            public Work()
+            {
+                Name = "";
+                Source = "";
+                Target = "";
+                Type = "";
+                State = "inactive";
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
 
-
-    }
 }
