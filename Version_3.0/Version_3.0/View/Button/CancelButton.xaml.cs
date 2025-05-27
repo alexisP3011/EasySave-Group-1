d487be79e4606fcac95b38fac65d9033e0e4af99
Elrazi <elrazi.koula@gmail.com>
2025-05-27 12:15:49 +0200

    creating a v3.0 solution

---
+++ Version_3.0/Version_3.0/View/Button/CancelButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Version_3._0.View.Button
+{
+    /// <summary>
+    /// Logique d'interaction pour CancelButton.xaml
+    /// </summary>
+    public partial class CancelButton : UserControl
+    {
+        public CancelButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Recherche la fenêtre parente et la ferme si elle existe
+            Window parentWindow = Window.GetWindow(this);
+            if (parentWindow != null)
+            {
+                parentWindow.Close();
+            }
+        }
+    }
+}
