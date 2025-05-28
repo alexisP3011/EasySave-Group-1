06f7004be1fe7143e41d87910432f0d2f7d9e6af
Clement <clement.evangelisti@viacesi.fr>
2025-05-28 14:53:36 +0200

    add IHM base interface and connection to server

---
+++ Version_3.0/IHM/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IHM
+{
+    class Client
+    {
+        public static void ConnectToServer()
+        {
+            try
+            {
+                // Adresse IP et port du serveur
+                IPAddress serverIp = IPAddress.Parse("127.0.0.1"); // ou l’IP du serveur distant
+                int port = 1448;
+
+                IPEndPoint serverEndPoint = new IPEndPoint(serverIp, port);
+
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                clientSocket.Connect(serverEndPoint);
+
+                MessageBox.Show("Connecté au serveur !");
+
+                // Pas d'action pour l'instant. On reste connecté.
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("❌ Erreur de connexion : " + ex.Message);
+            }
+        }
+    }
+}
